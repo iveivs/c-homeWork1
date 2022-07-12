0b3c2737678f9e876e1bfb241a4d4a47d8662223
veiv <veiv@mail.ru>
2022-07-13 02:53:37 +0300

    add solution task 4

---
+++ homeTask04/Program.cs
@@ -0,0 +1,23 @@
+﻿string? inputLine = Console.ReadLine();
+
+if(inputLine != null)
+{
+    int inputNumber = int.Parse(inputLine);
+    
+    int startNumber = 2;
+
+    string lineOutput = "";
+
+    while (startNumber < inputNumber)
+    {
+        lineOutput = lineOutput + startNumber + ", ";
+
+        startNumber = startNumber + 2;
+
+    }
+
+    lineOutput = lineOutput + inputNumber;
+
+   Console.WriteLine(lineOutput);
+
+}
