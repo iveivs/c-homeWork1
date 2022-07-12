c3b72cc4e5579aab1d0c66b0099fcc7fc6ea9758
veiv <veiv@mail.ru>
2022-07-13 01:16:24 +0300

    add solution task 3

---
+++ homeTask03/Program.cs
@@ -0,0 +1,17 @@
+﻿string? inputA = Console.ReadLine();
+
+if(inputA != null)
+{
+    int inputNumberA = int.Parse(inputA);
+    int outputNumberB = inputNumberA%2;
+
+    if(outputNumberB == 0)
+    {
+        Console.WriteLine("even");
+    }
+    else
+    {
+        Console.WriteLine("odd");
+    }
+
+}
