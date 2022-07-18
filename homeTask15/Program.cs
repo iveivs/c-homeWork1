efa06dfb7edc9e6cab4745283ea4cea02e9272d6
veiv <veiv@mail.ru>
2022-07-19 02:11:28 +0300

    add homeTask15

---
+++ homeTask15/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 15
+// Напишите программу, которая принимает на вход цифру, обозначающую день недели,
+// и проверяет, является ли этот день выходным.
+
+string? inputNumber = Console.ReadLine();
+
+int inputDay = int.Parse(inputNumber);
+
+if((inputDay == 6 ) || (inputDay == 7))
+{
+    Console.WriteLine("Выходной");
+}
+else
+{
+    Console.WriteLine("Будний день");
+}
+
