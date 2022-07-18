e0b22a7adac82c329ec2dda6137b8c17b58f74a8
veiv <veiv@mail.ru>
2022-07-19 01:43:26 +0300

    add homeTask13

---
+++ homeTask13/Program.cs
@@ -0,0 +1,16 @@
+﻿// Задача 13
+// Напишите программу, которая выводит третью цифру заданного числа  или сообщает,
+// что третьей цифры нет.
+
+string? inputFirst = Console.ReadLine();
+
+char[] digits = inputFirst.ToCharArray();
+
+if(digits.Length >= 3)
+{
+    Console.WriteLine(digits[2]);
+}
+else
+{
+    Console.WriteLine("Третьей цифры нет");
+}
