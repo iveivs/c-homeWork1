a48597d2e6af4ecd2a15c2ae0d6b087bdd175234
veiv <veiv@mail.ru>
2022-07-19 01:21:31 +0300

    add homeTask10

---
+++ homeTask10/Program.cs
@@ -0,0 +1,10 @@
+﻿// Напишите программу, которая принимает на вход трёхзначное число,
+// и на выходе показывает вторую цифру этого числа.
+
+string? inputFirst = Console.ReadLine();
+
+int inputNumberFirst = int.Parse(inputFirst);
+
+int inputNumberTwo = (inputNumberFirst % 100) / 10;
+
+Console.WriteLine(inputNumberTwo);
