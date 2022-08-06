d07a4701f7c9dcfb9b4af141853f2598439a6c3e
veiv <veiv@mail.ru>
2022-08-06 03:58:44 +0300

    add homeTask25

---
+++ homeTask25/Program.cs
@@ -0,0 +1,46 @@
+﻿// №25 Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в
+// натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+
+string? inputLineNumberA = Console.ReadLine();
+string? inputLineNumberB = Console.ReadLine();
+
+int inputNumberA;
+int inputNumberB;
+int outNumber;
+int i;
+
+void NumberToPow()
+{
+if(inputLineNumberA != null && inputLineNumberB != null)
+{
+    inputNumberA = int.Parse(inputLineNumberA);
+    inputNumberB = int.Parse(inputLineNumberB);
+
+    outNumber = (int)Math.Pow(inputNumberA, inputNumberB); 
+    Console.WriteLine(outNumber);
+}
+}
+
+NumberToPow();
+
+void NumberToPow2()
+{
+    if(inputLineNumberA != null && inputLineNumberB != null)
+    {
+        inputNumberA = int.Parse(inputLineNumberA);
+        inputNumberB = int.Parse(inputLineNumberB);
+        i = 0;
+        outNumber = 1;
+        while (i < inputNumberB)
+        {
+            outNumber = outNumber * inputNumberA;
+            i++;
+        }
+        Console.WriteLine(outNumber);
+    }
+}
+NumberToPow2();
+
