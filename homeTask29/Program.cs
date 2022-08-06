0a4f0dc64c2b80daaa2abf9075ce26c1a3f1e780
veiv <veiv@mail.ru>
2022-08-07 01:24:44 +0300

    add homeTask29

---
+++ homeTask29/Program.cs
@@ -0,0 +1,18 @@
+﻿// №29 Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+System.Random numberSintezator = new Random();
+
+void VariantNaive()
+{
+    int i = 0;
+    Console.Write("[");
+    while (i < 8-1)
+    {
+        Console.Write(numberSintezator.Next(0,10) + ",");
+        i++;   
+    }
+    Console.Write(numberSintezator.Next(0,2));
+    Console.Write("]");
+}
+
+VariantNaive();
