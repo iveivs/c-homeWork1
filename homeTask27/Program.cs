96e2d2aebf035ec7ded12ba01888c8f22908e2ef
veiv <veiv@mail.ru>
2022-08-07 01:09:00 +0300

    add homeTask27

---
+++ homeTask27/Program.cs
@@ -0,0 +1,26 @@
+﻿// №27 Напишите программу, которая принимает на вход число и выдаёт сумму цифр в
+// числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+string? inputLineNumberA = Console.ReadLine();
+
+int inputNumberA;
+int inputNumberB;
+
+void Conculate()
+{
+if(inputLineNumberA != null)
+{
+   inputNumberA = int.Parse(inputLineNumberA);
+   inputNumberB = 0;
+   while (inputNumberA > 0)
+   {
+      inputNumberB = inputNumberB + inputNumberA%10;
+      inputNumberA = inputNumberA/10;      
+   }
+   Console.WriteLine(inputNumberB);
+}
+}
+Conculate();
