5093bc6b9d1d6ade79adf01adc372a95a14dfbca
veiv <veiv@mail.ru>
2022-08-19 01:32:31 +0300

    add homeTask47

---
+++ homeTask47/Program.cs
@@ -0,0 +1,72 @@
+﻿// №47 Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+// * При выводе матрицы показывать каждую цифру разного цвета(цветов всего 16)
+
+
+int[,] FillTwoDimArray(int countRow, int countColumn)
+{
+    System.Random numberSintezator = new System.Random();
+
+    int i = 0;
+    int j = 0;
+    int[,] outArray = new int[countRow, countColumn];
+    while(i < countRow)
+    {
+        j = 0;
+        while(j < countColumn)
+        {
+            outArray[i,j] = numberSintezator.Next(0,100);
+            j++;
+        }
+        i++;
+    }
+    return outArray;
+}
+
+
+
+int[,] UpdateTwoDimArray(int[,] inputArray)
+{
+    int i = 0;
+    int j = 0;
+    while(i < inputArray.GetLength(0))
+    {
+        j = 0;
+        while(j < inputArray.GetLength(1))
+        {
+            inputArray[i,j] = Random.NextDouble() * 100;  // Вопрос: Получается в этой строке я создаю массив
+                                                        // с вещественными числами, но ведь возвращаю я в метод int(
+                                                         //   так как в начале перед иминем метода я указал "int")
+            j++;
+        }
+        i++;
+    }
+    return inputArray;
+}
+
+// Метод вывода массива на печать
+void PrintTwoDimArray(int[,] inputArray)
+{
+    int i = 0;
+    int j = 0;
+    while(i < inputArray.GetLength(0))
+    {
+        j = 0;
+        while(j < inputArray.GetLength(1))
+        {
+            Console.Write(inputArray[i,j] + " ");
+            j++;
+        }
+        // Console.Write("\n")
+        Console.WriteLine();
+        i++;
+    }
+    
+}
+
+int[,] twoDimArray = FillTwoDimArray(5,8);
+UpdateTwoDimArray(twoDimArray);
+PrintTwoDimArray(twoDimArray);
