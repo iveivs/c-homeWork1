2cea71a63250388487e52034d9b997a4d6d20fad
veiv <veiv@mail.ru>
2022-08-19 03:26:56 +0300

    add homeTask52

---
+++ homeTask52/Program.cs
@@ -0,0 +1,86 @@
+﻿// №52 Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом
+// столбце.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+
+int[,] FillTwoDimArray(int countRow, int countColumn)
+{
+    System.Random numberSintezator = new System.Random();
+
+    int i = 0;
+    int j = 0;
+    int[,] outArray = new int[countRow, countColumn];
+    while(i < countRow)
+    {
+        j = 0;
+        while(j < countColumn)
+        {
+            outArray[i,j] = numberSintezator.Next(0,100);
+            j++;
+        }
+        i++;
+    }
+    return outArray;
+}
+
+// Метод вывода массива на печать
+void PrintTwoDimArray(int[,] inputArray)
+{
+    int i = 0;
+    int j = 0;
+    while(i < inputArray.GetLength(0))
+    {
+        j = 0;
+        while(j < inputArray.GetLength(1))
+        {
+            Console.Write(inputArray[i,j] + " ");
+            j++;
+        }
+        // Console.Write("\n")
+        Console.WriteLine();
+        i++;
+    }
+    
+}
+
+double[,] CalculateTwoDimArray(int[,] inputArray)
+{
+    double outArray = new double[inputArray.GetLength(0),inputArray.GetLength(1)];
+    int i = 0;
+    int j = 0;
+    while(i < inputArray.GetLength(0))
+    {
+        j = 0;
+        while(j < inputArray.GetLength(1))
+        {
+            outArray[j] += inputArray[i,j]; 
+            j++;
+        }
+        i++;
+    }
+    return outArray;
+}
+
+double Count(double[,] outArray, int s)
+{
+    int i = 0;
+    int j = 0;
+    while(i < inputArray.GetLength(0))
+    {
+        j = 0;
+        while(j < inputArray.GetLength(1))
+        {
+            outArray[i] =  outArray[i] / s;
+            j++;
+        }
+        i++;
+    }
+    return outArray;
+}
+
+int[,] twoDimArray = FillTwoDimArray(5,8);
+PrintTwoDimArray(twoDimArray);
+Count(CalculateTwoDimArray())
