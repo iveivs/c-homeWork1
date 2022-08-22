ad30d5ae8f1c500d1deb7ef65df62a9947f37c5b
veiv <veiv@mail.ru>
2022-08-22 15:00:00 +0300

    add homeTask02

--- homeTask02/Program.cs
+++ homeTask02/Program.cs
@@ -1,4 +1,8 @@
-﻿string? inputLineOne = Console.ReadLine();
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+
+string? inputLineOne = Console.ReadLine();
 string? inputLineTwo = Console.ReadLine();
 string? inputLineThree = Console.ReadLine();
 
