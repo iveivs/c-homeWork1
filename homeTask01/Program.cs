08f1622ecfec0b7cf9013709e9ceadd9ec717dcd
veiv <veiv@mail.ru>
2022-08-22 14:55:42 +0300

    add homeTask01

--- homeTask01/Program.cs
+++ homeTask01/Program.cs
@@ -1,4 +1,10 @@
-﻿string? inputLineOne = Console.ReadLine();
+﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+// a = 5; b = 7 -> max = 7
+// a = 2 b = 10 -> max = 10
+
+// Решение:
+
+string? inputLineOne = Console.ReadLine();
 string? inputLineTwo = Console.ReadLine();
 
 if(inputLineOne != null && inputLineTwo != null)
