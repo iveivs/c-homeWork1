7b91ca9d3317d7cee2f334a9f5e8d6ba2876b6c5
veiv <veiv@mail.ru>
2022-08-25 03:37:39 +0300

    add homeTask64mySolution

---
+++ homeTask64mySolution/Program.cs
@@ -0,0 +1,27 @@
+﻿//  Задача 64: 
+//
+//  Задайте значение N. Напишите программу, которая выведет все натуральные числа в промежутке от N до 1.
+//  N = 5 -> "5, 4, 3, 2, 1"
+//  N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+
+Console.Clear();
+
+int ReadData()
+{
+    Console.WriteLine("Задайте число: ");
+    return int.Parse(Console.ReadLine());
+}
+
+void NaturalNumberPrinter(int num)
+{
+    // Делаем остановку нашего метода
+    if(num == 0) return;
+    // Выводим на печать текущее число
+    Console.Write(num + " ");
+    // Вызываем метод и уменьшаем число на 1
+    NaturalNumberPrinter(num-1);
+    
+}
+
+int inputNumber = ReadData();
+NaturalNumberPrinter(inputNumber);
